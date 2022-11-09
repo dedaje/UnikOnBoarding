028beff8d111385529ea6d17ad71e9ac0338188d
Rasmus Høy Schmidt <rasshoey@gmail.com>
2022-11-09 14:30:09 +0100

    Nuget packages installeret

--- UnikOnBoarding/Program.cs
+++ UnikOnBoarding/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UnikOnBoarding.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WebAppUserDbContextConnection") ?? throw new InvalidOperationException("Connection string 'WebAppUserDbContextConnection' not found.");
+
+builder.Services.AddDbContext<WebAppUserDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<WebAppUserDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
