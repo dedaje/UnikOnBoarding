d6e30238e90b4c25cbb57fb808a67d7854264a1d
dedaje <mhel38989@edu.ucl.dk>
2023-01-04 11:46:13 +0100

    fre

--- WebClient.UnikOnboarding/Program.cs
+++ WebClient.UnikOnboarding/Program.cs
@@ -1,61 +1,55 @@
-namespace WebClient.UnikOnBoarding
+using System;
+
+using System.Net.Http;
+
+using System.Threading;
+
+using System.Threading.Tasks;
+
+namespace ConsoleApp59WebClient
+
 {
 
-    public class Program
+    internal class Program
+
     {
-        public Program() // Dependency injects each service frontend which should be similar-ish to this one
-        {
-            
-        }
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
+
         {
-            string[] services = { "Booking", "Project", "ProjectUsers", "Task", "User" };
-            foreach (var service in services)
+
+            // Sync:
+
+            string url = "https://localhost:7208/api/User/AllUsers";
+
+            System.Net.WebClient w = new System.Net.WebClient();
+
+            Console.WriteLine("Sync: " + w.DownloadString(new Uri(url)));
+
+            // ASync:
+
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            tokenSource = new CancellationTokenSource();
+
+            CancellationToken token = tokenSource.Token;
+
+            using (HttpClient client = new HttpClient())
+
             {
-                var index = Array.FindIndex(services, nr => nr.Contains(service));
-                Console.WriteLine($"{index.ToString()}: {service}");
-                Console.WriteLine("");
+
+                token.ThrowIfCancellationRequested();
+
+                HttpResponseMessage r = await client.GetAsync(url, token);
+
+                Console.WriteLine("ASync: " + await r.Content.ReadAsStringAsync());
+
             }
 
-            int value;
-            string number;
-            do
-            {
-                Console.Write("Choose Service (number): ");
-                number = Console.ReadLine();
-            } while ((!string.IsNullOrEmpty(number)) || (!int.TryParse(number, out value)));
+            Console.ReadLine();
 
-            value = Convert.ToInt32(number);
-
-            switch (value)
-            {
-                default:
-                    Console.WriteLine("No number was selected.");
-                    break;
-                
-                // Each case calls a method, which then gives the user a list of each method that every service has, to which the has to pick one.
-                // Each method should return a list to show the result of the performed action.
-                case 0:
-                    //
-                    break;
-
-                case 1:
-                    //
-                    break;
-
-                case 2:
-                    //
-                    break;
-
-                case 3:
-                    //
-                    break;
-
-                case 4:
-                    //
-                    break;
-            }
         }
+
     }
+
 }
