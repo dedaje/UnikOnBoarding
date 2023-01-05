931f8b2d063567f90e204a7db123b91ac52f9614
Rasmus Høy Schmidt <rasshoey@gmail.com>
2023-01-05 20:43:33 +0100

    fgh

--- WebClient.UnikOnboarding/Program.cs
+++ WebClient.UnikOnboarding/Program.cs
@@ -1,3 +1,8 @@
+using System.Net.Http.Json;
+using WebClient.UnikOnBoarding.Infrastructure.Contract;
+using WebClient.UnikOnBoarding.Infrastructure.Contract.Dto.User;
+using WebClient.UnikOnBoarding.Infrastructure.Implementation;
+
 namespace WebClient.UnikOnBoarding
 {
 
@@ -5,57 +10,85 @@ namespace WebClient.UnikOnBoarding
     {
         public Program() // Dependency injects each service frontend which should be similar-ish to this one
         {
-            
         }
 
-        static void Main(string[] args)
+        static async Task /*void*/ Main(string[] args)
         {
-            string[] services = { "Booking", "Project", "ProjectUsers", "Task", "User" };
-            foreach (var service in services)
+            //string[] services = { "Booking", "Project", "ProjectUsers", "Task", "User" };
+            //foreach (var service in services)
+            //{
+            //    var index = Array.FindIndex(services, nr => nr.Contains(service));
+            //    Console.WriteLine($"{index.ToString()}: {service}");
+            //    Console.WriteLine("");
+            //}
+
+            //int value;
+            //string number;
+            //do
+            //{
+            //    Console.Write("Choose Service (number): ");
+            //    number = Console.ReadLine();
+            //} while ((!string.IsNullOrEmpty(number)) || (!int.TryParse(number, out value)));
+
+            //value = Convert.ToInt32(number);
+
+            //switch (value)
+            //{
+            //    default:
+            //        Console.WriteLine("No number was selected.");
+            //        break;
+
+            //    // Each case calls a method, which then gives the user a list of each method that every service has, to which the has to pick one.
+            //    // Each method should return a list to show the result of the performed action.
+            //    case 0:
+            //        //
+            //        break;
+
+            //    case 1:
+            //        //
+            //        break;
+
+            //    case 2:
+            //        //
+            //        break;
+
+            //    case 3:
+            //        //
+            //        break;
+
+            //    case 4:
+            //        //
+            //        break;
+            //}
+            // Sync:
+
+            string url = "https://localhost:7208/api/User/AllUsers";
+
+            //System.Net.WebClient w = new System.Net.WebClient();
+
+            //Console.WriteLine("Sync: " + w.DownloadString(new Uri(url)));
+
+            // ASync:
+
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            tokenSource = new CancellationTokenSource();
+
+            CancellationToken token = tokenSource.Token;
+
+            using (HttpClient client = new HttpClient())
+
             {
-                var index = Array.FindIndex(services, nr => nr.Contains(service));
-                Console.WriteLine($"{index.ToString()}: {service}");
-                Console.WriteLine("");
+
+                token.ThrowIfCancellationRequested();
+
+                HttpResponseMessage r = await client.GetAsync(url, token);
+
+                Console.WriteLine("ASync: \n" + await r.Content.ReadAsStringAsync());
+
             }
 
-            int value;
-            string number;
-            do
-            {
-                Console.Write("Choose Service (number): ");
-                number = Console.ReadLine();
-            } while ((!string.IsNullOrEmpty(number)) || (!int.TryParse(number, out value)));
-
-            value = Convert.ToInt32(number);
-
-            switch (value)
-            {
-                default:
-                    Console.WriteLine("No number was selected.");
-                    break;
-                
-                // Each case calls a method, which then gives the user a list of each method that every service has, to which the has to pick one.
-                // Each method should return a list to show the result of the performed action.
-                case 0:
-                    //
-                    break;
-
-                case 1:
-                    //
-                    break;
-
-                case 2:
-                    //
-                    break;
-
-                case 3:
-                    //
-                    break;
-
-                case 4:
-                    //
-                    break;
-            }
+            Console.ReadLine();
         }
     }
 }
